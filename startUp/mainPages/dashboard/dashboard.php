0619acb03e8412dee74d152972e1f58d1eaa02f4
Lazar-Bogdan <bogdilazar5@gmail.com>
2021-09-30 18:42:03 +0300

    crud finished, maybe tested more ? (for better security, verify if labels are empty )

--- startUp/mainPages/dashboard/dashboard.php
+++ startUp/mainPages/dashboard/dashboard.php
@@ -8,24 +8,26 @@
     </head>
 
     <body>
-
-        <form method="POST" action="dashboardService.php">
-            <p> Welcome ! </p>
-            <input type="text" name="nameSeason">
-            <input type="text" name="teamName">
-            <input type="text" name="matches">
-            <input type="text" name="wins">
-            <input type="text" name="loses">
-            <input type="text" name="ties">
-            <input type="text" name="goals">
-            <input type="text" name="points">
-            <button type="submit" name="confirm">Add</button>
-
-
-        </form>
-
+        <?php require_once 'dashboardService.php'?>
 
         <?php
+
+            if(isset($_SESSION['message'])):?>
+
+            <div class="alert alert-<?=$_SESSION['msg_type']?>">
+
+            <?php
+                echo $_SESSION['message'];
+                unset($_SESSION['message']);
+            ?>
+            </div>
+
+
+        <?php endif ?>
+
+        <form method="POST" action="dashboardService.php">
+
+            <?php
 
             $mysqli = new mysqli('localhost','root','','user_database') or die("unable to connect");
             $result = $mysqli->query("SELECT * FROM teams") or die($mysqli->error);
@@ -36,40 +38,77 @@
             //    print_r($array);
             //    echo '</pre>';
             //}
+
+            ?>
+
+
+            <div class="row justify-content-center">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>nameSeason</th>
+                            <th>teamName</th>
+                            <th>matches</th>
+                            <th>wins</th>
+                            <th>loses</th>
+                            <th>ties</th>
+                            <th>goals</th>
+                            <th>points</th>
+                            <th colspan="2">Action</th>
+                        </tr>
+                    </thead>
+            <?php
+                while($row= $result->fetch_assoc()) :
+            ?>
+                <tr>
+                    <td><?php echo $row['nameSeason']; ?></td>
+                    <td><?php echo $row['teamName']; ?></td>
+                    <td><?php echo $row['matches']; ?></td>
+                    <td><?php echo $row['wins']; ?></td>
+                    <td><?php echo $row['loses']; ?></td>
+                    <td><?php echo $row['ties']; ?></td>
+                    <td><?php echo $row['goals']; ?></td>
+                    <td><?php echo $row['points']; ?></td>
+                    <td>
+                        <a href="dashboard.php?edit= <?php echo $row['position'];?>"
+                            class="btn btn-danger">Edit</a>
+                        <a href="dashboardService.php?delete=<?php echo $row['position'];?>"
+                            class="btn btn-danger">Delete</a>
+                    </td>
+                </tr>
+                <?php endwhile;?>
+                </table>
+            </div>
+
+
+            <p> Welcome ! </p>
+            <input type="hidden" name="position" value="<?php echo $position; ?>">
+            <input type="text" value="<?php echo $nameSeason?>" name="nameSeason">
+            <input type="text" value="<?php echo $teamName?>" name="teamName">
+            <input type="text" value="<?php echo $matches?>" name="matches">
+            <input type="text" value="<?php echo $wins?>" name="wins">
+            <input type="text" value="<?php echo $loses?>" name="loses">
+            <input type="text" value="<?php echo $ties?>" name="ties">
+            <input type="text" value="<?php echo $goals?>" name="goals">
+            <input type="text" value="<?php echo $points?>" name="points">
+            <?php
+                if($update==true):
+            ?>
+            <button type="submit" name="update">Update</button>
+            <?php else: ?>
+            <button type="submit" name="confirm">Confirm</button>
+            <?php endif;?>
             
-        ?>
+
+            
+
+
+        </form>
+
+
         
-        <div class="row justify-content-center">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>nameSeason</th>
-                        <th>teamName</th>
-                        <th>matches</th>
-                        <th>wins</th>
-                        <th>loses</th>
-                        <th>ties</th>
-                        <th>goals</th>
-                        <th>points</th>
-                        <th>position</th>
-                    </tr>
-                </thead>
-        <?php
-            while($row= $result->fetch_assoc()) :
-        ?>
-            <tr>
-                <td><?php echo $row['nameSeason']; ?></td>
-                <td><?php echo $row['teamName']; ?></td>
-                <td><?php echo $row['matches']; ?></td>
-                <td><?php echo $row['wins']; ?></td>
-                <td><?php echo $row['loses']; ?></td>
-                <td><?php echo $row['ties']; ?></td>
-                <td><?php echo $row['goals']; ?></td>
-                <td><?php echo $row['points']; ?></td>
-            </tr>
-            <?php endwhile;?>
-            </table>
-        </div>
+        
+        
 
         <?php
 
--- startUp/mainPages/dashboard/dashboardService.php
+++ startUp/mainPages/dashboard/dashboardService.php
@@ -1,22 +1,73 @@
 <?php
 
+    session_start();
+
     $connect = new mysqli('localhost','root','','user_database') or die("unable to connect");
-
-
-    $nameSeason = $_POST['nameSeason'];
-    $teamName = $_POST['teamName'];
-    $matches=$_POST['matches'];
-    $wins=$_POST['wins'];
-    $loses=$_POST['loses'];
-    $ties=$_POST['ties'];
-    $goals=$_POST['goals'];
-    $points=$_POST['points'];
+    #$update=true;
 
     if(isset($_POST['confirm'])){
+        $nameSeason = $_POST['nameSeason'];
+        $teamName = $_POST['teamName'];
+        $matches=$_POST['matches'];
+        $wins=$_POST['wins'];
+        $loses=$_POST['loses'];
+        $ties=$_POST['ties'];
+        $goals=$_POST['goals'];
+        $points=$_POST['points'];
         $mysql = "insert into teams (nameSeason,teamName,matches,wins,loses,ties,goals,points) values ('$nameSeason','$teamName','$matches','$wins','$loses','$ties','$goals','$points') ";
         $query=mysqli_query($connect,$mysql);
+        $_SESSION['message']= "Record has been added";
+        $_SESSION['msg_type']="succes";
         header("Location:dashboard.php");
     }
 
+
+
+    if(isset($_GET['delete'])){
+        $position=$_GET['delete'];
+        $rez1=$connect->query("DELETE FROM teams WHERE position='$position'") or die("unable to connect");
+        $_SESSION['message']= "Record has been removed";
+        $_SESSION['msg_type']="danger";
+        header("location:dashboard.php");   
+    }
+
+
+    if(isset($_GET['edit'])){
+        $position=$_GET['edit'];
+        $rez = $connect->query("SELECT * FROM teams WHERE position='$position'")or die("unable to connect");
+        $update=true;
+        if($rez->num_rows!=0){
+            $row = $rez->fetch_array();
+            $nameSeason=$row['nameSeason'];
+            $teamName = $row['teamName'];
+            $matches=$row['matches'];
+            $wins=$row['wins'];
+            $loses=$row['loses'];
+            $ties=$row['ties'];
+            $goals=$row['goals'];
+            $points=$row['points'];
+        }
+    }
+
+    if(isset($_POST['update'])){
+        $position=$_POST['position'];
+        $nameSeason = $_POST['nameSeason'];
+        $teamName = $_POST['teamName'];
+        $matches=$_POST['matches'];
+        $wins=$_POST['wins'];
+        $loses=$_POST['loses'];
+        $ties=$_POST['ties'];
+        $goals=$_POST['goals'];
+        $points=$_POST['points'];
+
+        $connect->query("UPDATE teams set nameSeason='$nameSeason', teamName='$teamName',
+            matches='$matches', wins='$wins', loses='$loses',
+            ties='$ties', goals='$goals', points='$points' WHERE position='$position'") or die ("unable to connect");
+        
+        $_SESSION['message']= "Record has been updated";
+        $_SESSION['msg_type']="warning";
+        
+        header("location: dashboard.php");
+    }
 
 ?>
