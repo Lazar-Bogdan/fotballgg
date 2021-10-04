15f16121d3a8b9cb9db38f479c8ab2b32e3b89a3
Lazar-Bogdan <bogdilazar5@gmail.com>
2021-10-04 14:25:12 +0300

    added css style on register

--- startUp/register/register.php
+++ startUp/register/register.php
@@ -8,19 +8,29 @@
     </head>
 
     <body>
+        <link rel="stylesheet" href="registerstyle.css">
 
+        <main id="main-holder">
+            <div id="register-error-msg-holder">
+                <p id="register-error-msg">Invalid username <span id="error-msg-second-line">and/or password</span></p>
+            </div>
 
-        <form action="registerService.php" method="POST">
-            <input type="text" placeholder="name" name="name">
-            <input type="text" placeholder="username" name="username">
-            <input type="text" placeholder="password" name="password">
-            <input type="text" placeholder="gmail" name="gmail">
-            <input type="text" placeholder="country" name="country">
-            <input type="text" placeholder="state" name="state">
-            <button type="submit" name="register">register</button>
-            <button type="submit" name="login">Go To Login</button>
+            <form action="registerService.php" method="POST" id="register-form">
+                <div class="register-label">
+                    <input type="text" placeholder="name" name="name">
+                    <input type="text" placeholder="username" name="username">
+                    <input type="text" placeholder="password" name="password">
+                    <input type="text" placeholder="gmail" name="gmail">
+                    <input type="text" placeholder="country" name="country">
+                    <input type="text" placeholder="state" name="state">
+                </div>
+                <p></p>
+                <button type="submit" name="register" class="button-style">register</button>
+                <p></p>
+                <button type="submit" name="login" class="button-style">Go To Login</button>
 
-        </form>
+            </form>
+        </main>
 
 
     </body>
